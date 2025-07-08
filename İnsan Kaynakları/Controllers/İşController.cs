88290d0e880563ae503f894d44755f42b4d7de3b
Exfor <Exfor@MSI>
2025-07-08 14:48:48 +0300

    Proje dosyası ekle.

---
+++ İnsan Kaynakları/Controllers/İşController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace İnsan_Kaynakları.Controllers
+{
+    public class İşController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            ViewBag.Isbaslık1 = "Yazılım Mühendisi";
+            ViewBag.Sirket1 = "Yazılım şirketi";
+            ViewBag.Isdetay1 = "Yanımızda çalışacak genç yetenek arıyoruz";
+
+            ViewBag.Isbaslık2 = "Pazarlama Uzmanı";
+            ViewBag.Sirket2 = "Digital Marketing Co";
+            ViewBag.Isdetay2 = "Sosyal Medya içeriği ve hesapları yönet";
+
+            return View();
+        }
+    }
+}
